2959fbb6319f498b220f4f8660e1e880a97078c9
Andrea <andreqtqng@gmail.com>
2025-03-07 15:49:32 +0000

    commit

--- UniSphereApp/models.py
+++ UniSphereApp/models.py
@@ -1,30 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
-
-
-from django.db import models
-#impoered from django's built-in user model (check with jiacheng)
-from django.contrib.auth.models import User
-
-class StudentPost(models.Model):
-    #link the post to the user (check with jiacheng and reem for the users)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    caption = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        #check the slicing to 30
-        #displat username + caption in admin
-        return f"{self.username} - {self.caption[:30]}"
-
-class PostFile(models.Model):
-    #link files to the post
-    post = models.ForeignKey(StudentPost, related_name="files", on_delete=models.CASCADE)
-    #uploads diles to "upload/" directory
-    file = models.FileField(upload_to="uploads/")
-
 
 class User(AbstractUser):
     STUDENT = 'student'
@@ -37,4 +14,24 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+class StudentPost(models.Model):
+    #link the post to the user (check with jiacheng and reem for the users)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    caption = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        #check the slicing to 30
+        #displat username + caption in admin
+        return f"{self.user.username} - {self.caption[:30]}"
+
+class PostFile(models.Model):
+    #link files to the post
+    post = models.ForeignKey(StudentPost, related_name="files", on_delete=models.CASCADE)
+    #uploads diles to "upload/" directory
+    file = models.FileField(upload_to="uploads/")
+
+
+
 
