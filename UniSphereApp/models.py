e634233888098ea6a09bd0fbebc9cbd9a7fa5b83
Rem2525 <remmehyar@gmail.com>
2025-03-13 15:38:31 +0000

    added social features
    added like, comment, share, friendrequest models to UniSphereApp/models.py should all be working fine.

--- UniSphereApp/models.py
+++ UniSphereApp/models.py
@@ -58,4 +58,45 @@ class StudentProfile(models.Model):
     def __str__(self):
         return self.user.username
 
+# New code adding on social features
+
+class Comment(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    post = models.ForeignKey('StudentPost', on_delete=models.CASCADE, related_name="comments")
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.user.username} commented on {self.post.title[:30]}"
+
+class Like(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    post = models.ForeignKey('StudentPost', on_delete=models.CASCADE, related_name="likes")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} liked {self.post.title[:30]}"
+
+
+class Share(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    post = models.ForeignKey('StudentPost', on_delete=models.CASCADE, related_name="shares")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} shared {self.post.title[:30]}"
+
+class FriendRequest(models.Model):
+    from_user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="sent_requests", on_delete=models.CASCADE)
+    to_user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="received_requests", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    accepted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Friend Request from {self.from_user.username} to {self.to_user.username}"
+
+
+
+
+
 
